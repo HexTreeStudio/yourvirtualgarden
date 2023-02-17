b143ebb83f77a12d46349857d1abb3daf75f257c
HexTreeStudio <lukasplesnik@gmail.com>
2023-02-17 09:13:04 +0100

    bugs

--- src/components/TopLabel.js
+++ src/components/TopLabel.js
@@ -2,7 +2,7 @@ import label from "../img/label_1.webp";
 function TopLabel({ value, icon }) {
   return (
     <div className="w-40 max-lg:w-32 max-md:w-28 relative max-[374px]:w-20 ">
-      <img src={label} alt="label" />
+      <img src={label} alt="label" className="w-full h-full" />
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div className="flex justify-between items-center w-24 max-lg:w-20 max-[374px]:w-14">
           {icon}
